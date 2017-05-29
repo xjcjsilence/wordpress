75a00eb0b2df0b692100c627345f9892aa5eca46
张涛 <383299590@qq.com>
2017-05-29 23:44:06 +0800

    update

--- wp-config.php
+++ wp-config.php
@@ -26,10 +26,10 @@ define('DB_NAME', 'wordpress');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'mysqlForUimaster');
 
 /** MySQL hostname */
-define('DB_HOST', 'mysql');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
