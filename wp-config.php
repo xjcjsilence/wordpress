c3a6e421cdfdf13bda05bcfa9438a9f800a53ae4
张涛 <383299590@qq.com>
2017-05-30 11:23:01 +0800

    update

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'wordpress');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'mysqlForUimaster');
 
 /** MySQL hostname */
 define('DB_HOST', 'mysql');
